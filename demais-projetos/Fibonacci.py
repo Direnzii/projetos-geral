bbe88337a14d89a832e8d612cba0926c26ef039c
Direnzi <98608748+Direnzii@users.noreply.github.com>
2022-12-27 12:25:34 -0300

    Create Fibonacci.py

--- demais-projetos/Fibonacci.py
+++ demais-projetos/Fibonacci.py
@@ -1,18 +1,19 @@
-# mostrar = int(input('Quantos digitos ? '))
-#
-# n1 = 0
-# n2 = 1
-#
-# print(f'{n1}, {n2}, ', end='') #Mostro o 0, 1 pois sempre serão o inicio da sequencia
-#
-# contador = 2 #Inicio o contador no 2 pois ja possuo os 2 primeiros algarismos
-#
-# while contador < mostrar: #Inicio aqui o laço que é responsavel por printar o n3 na posição correta e na sequencia igualar o n1 a n2 e n2 a n3
-#     n3 = n1 + n2
-#     print(f'{n3}, ', end='') #O end é usado para o print não pular linha
-#     mostrar -= 1
-#     n1 = n2
-#     n2 = n3
-# print('FIM')
+mostrar = int(input('Quantos digitos ? '))
 
-# def calcular(numeros):
+n1 = 0
+n2 = 1
+
+print(f'{n1}, {n2}, ', end='') #Mostro o 0, 1 pois sempre serão o inicio da sequencia
+
+contador = 2 #Inicio o contador no 2 pois ja possuo os 2 primeiros algarismos
+
+while contador < mostrar: #Inicio aqui o laço que é responsavel por printar o n3 na posição correta e na sequencia igualar o n1 a n2 e n2 a n3
+    n3 = n1 + n2
+    print(f'{n3}, ', end='') #O end é usado para o print não pular linha
+    mostrar -= 1
+    n1 = n2
+    n2 = n3
+    
+print('FIM')
+
+def calcular(numeros):
